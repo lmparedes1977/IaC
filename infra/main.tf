d5355d92c142b351220d26863aa7bc56d4bb9a1c
lmparedes1977 <lmparedes@gmail.com>
2025-02-10 21:41:39 -0300

    updates

---
+++ infra/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  profile = "default"
+  region = var.regiao_aws
+}
+
+resource "aws_instance" "app_server" {
+  ami = "ami-0cb91c7de36eed2cb"
+  instance_type = var.instancia
+  key_name = var.chave
+  tags = {
+    Name = "Instancia com python e virtualenv"
+  }
+
+}
+
+resource "aws_key_pair" "chaveSSH" {
+  key_name = var.chave
+  public_key = file("${var.chave}.pub")
+}
